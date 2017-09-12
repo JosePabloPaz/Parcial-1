2151873b94deab652f74b758e005e5792c129271
JosePabloPaz <31837687+JosePabloPaz@users.noreply.github.com>
2017-09-11 23:33:43 -0600

    Entrega Parcial 1

---
+++ Parcial1/Parcial1/src/FabricaRanger.java
@@ -0,0 +1,10 @@
+
+public interface FabricaRanger {
+	
+	public void apilar(Monstruo monstruo);
+	public void desapilar(Monstruo monstruo);
+	public void encolar(Monstruo monstruo);
+	public void desencolar(Monstruo monstruo); 
+	public void fabricar(String elemento);
+	
+}
